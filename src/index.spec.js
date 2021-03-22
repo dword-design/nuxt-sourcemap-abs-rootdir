e3ba4339388d4590c32966ca5bbdc1efa42a2c83
Sebastian Landwehr <info@dword-design.de>
2021-03-22 16:18:48 +0100

    chore: fix test

--- src/index.spec.js
+++ src/index.spec.js
@@ -23,7 +23,7 @@ export default tester({
           |> await
           |> property('data')
         const sourceMappingUrlMatch = content.match(
-          /\\n\/\/# sourceMappingURL=data:application\/json;charset=utf-8;base64,(.*)\\n/
+          /\\n\/\/# sourceMappingURL=data:application\/json;charset=utf-8;base64,(.*?)\\n/
         )
         const sourceMapping = JSON.parse(
           Buffer.from(sourceMappingUrlMatch[1], 'base64').toString('utf-8')
